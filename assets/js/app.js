5a674c36124eddbca257aec4b1187a948b5fa1d8
Anya L Keller <kelleranyal@gmail.com>
2019-08-16 20:04:25 -0400

    asdf

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+//HTML ELEMENTS
+//Question Info
+var questinSpan = $("#question");
+
+//GLOBALS
+var questions = []; //the questions and answers, [{question:asdf,options:[option1,2,3,4],answer:asfd,soluion:asdf}]
